4500225a8435acb6036e6c8cc0a9209ea213bd1f
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2024-03-19 22:07:30 +0530

    Add leaflet-control-geocoder and leaflet-routing-machine dependencies

--- src/Pages/Add.jsx
+++ src/Pages/Add.jsx
@@ -1,157 +1,301 @@
-import React, { useEffect, useState } from "react"
-import { ArrowPathIcon } from '@heroicons/react/24/solid'
-import Base from "../Components/Base"
-import Card from "../Components/Card/Card"
-import { addBus, getBusById, updateBus } from "../Helpers"
-import { useNavigate, useSearchParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
+import Base from "../Components/Base";
+import Card from "../Components/Card/Card";
+import { addBus, getBusById, updateBus } from "../Helpers";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  LatLng,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import "leaflet-control-geocoder";
+
+import { createControlComponent } from "@react-leaflet/core";
 
 const Add = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [points, setPoints] = useState([]);
+  const [position, setPosition] = useState(null);
+  const [locationNames, setLocationNames] = useState([]);
 
-    const [data, setData] = useState({ })
-    const [loading ,setLoading] = useState(false)
-    const [error, setError] = useState(false)
-    const [message, setMessage] = useState(null)
-    const [params, setParams] = useSearchParams()
-    const navigate = useNavigate()
+  const handleMapClick = (e) => {
+    const { lat, lng } = e.latlng;
+    setPoints([...points, { lat, lng }]);
+    console.log(points);
+  };
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click(e) {
+        const latlng = e.latlng;
+        map.latLngToLayerPoint(latlng);
+        console.log(latlng);
+        // add latlng into array
+        setPoints([...points, latlng]);
+        console.log(points);
 
-    useEffect(() => {
-        if(params.has('id')) {
-            getBusById(params.get('id'))
-                .then((daa) => {
-                    setData(daa.dbRes)
-                })
-                .catch(err => console.log(err))
-        }
-        if(params.has('success')) {
-            if(params.get('success') === 'add') {
-                setMessage("Successfully Added New Bus!")
-            }
-            if(params.get('success') === 'update') {
-                setMessage("Successfully Updated Bus!")
-            }
-        }
-    })
-    const formSubmit = async (e) => {
-        e.preventDefault()
-        setLoading(true)
-        
-        try {
+        // Reverse geocoding to get location name locate
+        const geocoder = L.Control.Geocoder.nominatim();
+        geocoder.reverse(latlng, map.options.crs.scale(map.getZoom()), (results, status) => {
+          console.log(results);
+          const locationName = results[0].name;
+          // add location name into array
+          setLocationNames([...locationNames, locationName]);
+          console.log(locationNames);
+        });
+      },
+      locationfound(e) {
+        console.log(e);
+        setPosition(e.latlng);
+        console.log(e.latlng);
+      },
+    });
 
-            if(params.get('id')) {
-                const update = await updateBus(data, params.get('id'))
-                if(update.success) {
-                    setData({ })
-                    setLoading(false)
-                    return navigate('/add-bus?success=update')
-                }else {
-                    setError(true)
-                    setLoading(false)
-                }
-            }else {
-                // let array = data.viaPassPoints.split(',')
-                // setData({ ...data, viaPassPoints: array })
-                const add = await addBus(data)
-                if(add.success) {
-                    setData({ })
-                    setLoading(false)
-                    return navigate('/add-bus?success=add')
-                }else {
-                    setError(true)
-                    setLoading(false)
-                }
-            }
-        }catch(err) {
-            setLoading(false)
-            setError(true)
-        }
+    return position ? (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    ) : null;
+  };
+  //   useEffect(() => {
+  //     const map = L.map("map").setView([51.505, -0.09], 13);
+  //     map.on("click", handleMapClick);
+
+  //     return () => {
+  //       map.off("click", handleMapClick);
+  //     };
+  //   }, []); // Ensure this effect runs only once
+  // useEffect(() => {
+  //   const mapElement = document.getElementById("map");
+  //   if (!mapElement._leaflet_id) {
+  //     // Check if map is not initialized
+  //     const map = L.map(mapElement).setView([51.505, -0.09], 13);
+  //     map.on("click", handleMapClick);
+  //     return () => {
+  //       map.off("click", handleMapClick);
+  //     };
+  //   }
+  // }, []);
+  useEffect(() => {
+    if (params.has("id")) {
+      getBusById(params.get("id"))
+        .then((daa) => {
+          setData(daa.dbRes);
+        })
+        .catch((err) => console.log(err));
     }
+    if (params.has("success")) {
+      if (params.get("success") === "add") {
+        setMessage("Successfully Added New Bus!");
+      }
+      if (params.get("success") === "update") {
+        setMessage("Successfully Updated Bus!");
+      }
+    }
+  }, []);
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    return(
-        <Base>
-            <Card style="h-[82vh] w-[1600px] flex flex-col  p-2">
-                <h2 className="text-4xl font-bold ml-2 mt-2">
-                    { params.has('id') ? 'Update Bus' : 'Add Bus'}
-                </h2>
-                { message ? <p className="ml-2 mt-2 text-sm text-green-600 font-semibold">{message}</p>: ''}
-                <div className="m-5">
-                    <form onSubmit={formSubmit} className="flex flex-col space-y-1">
-                            <div className="container m-2 flex flex-col">
-                                <label for="route" className="font-semibold text-sm p-2">Route Name *</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, routeName: e.target.value })} 
-                                    type="text"
-                                    id="route"
-                                    value={data.routeName ? data.routeName : ''}
-                                    placeholder="Eg- Route 2"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                            <div className="container m-2 flex flex-col">
-                                <label className="font-semibold text-sm p-2">Vehicle Registration *</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, vehicleRegistration: e.target.value })} 
-                                    type="text"
-                                    value={data.vehicleRegistration ? data.vehicleRegistration : ''}
-                                    placeholder="Eg- UK07PA8077"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                            <div className="container m-2 flex flex-col">
-                                <label className="font-semibold text-sm p-2">Start Point *</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, startPoint: e.target.value })} 
-                                    type="text"
-                                    value={data.startPoint ? data.startPoint : ''}
-                                    placeholder="Eg- Kandoli"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                            <div className="container m-2 flex flex-col">
-                                <label className="font-semibold text-sm p-2">End Point *</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, endPoint: e.target.value })} 
-                                    type="text"
-                                    value={data.endPoint ? data.endPoint : ''}
-                                    placeholder="Eg- Bidholi"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                            <div className="container m-2 flex flex-col">
-                                <label className="font-semibold text-sm p-2">Via Pass Points</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, routeName: e.target.value })} 
-                                    type="text"
-                                    value={data.viaPassPoints ? data.viaPassPoints : ''}
-                                    placeholder="Eg- Mandi Chowk, Balliwala, Ballupur ( Seperated By Comma )"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                            <div className="container m-2 flex flex-col">
-                                <label className="font-semibold text-sm p-2">API Key *</label>
-                                <input 
-                                    onChange={(e) => setData({ ...data, apiKey: e.target.value })} 
-                                    type="text"
-                                    value={data.apiKey ? data.apiKey : ''}
-                                    placeholder="Eg- ljut4k"
-                                    className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none" 
-                                />
-                            </div>
-                        { error ? <p className="text-sm text-red-600">Error Occured! Make Sure You Have Filled All The Details!</p> : ''}
-                        <button disabled={loading ? true : false} type="submit" className="btn w-[300px] h-[45px] bg-[#E9286D] text-center text-white text-xl uppercase m-2 hover:shadow-2xl flex justify-center items-center">
-                            { params.has('id') ? 'Update' : 'Add Bus'} 
-                            { !loading ? '' :
-                                <ArrowPathIcon 
-                                    className="ml-2 animate-spin" 
-                                    width={30} 
-                                    height={30} 
-                                />
-                            }
-                        </button>
-                    </form>
-                </div>
-            </Card>
-        </Base>
-    )
-}
+    try {
+      if (params.get("id")) {
+        const update = await updateBus(data, params.get("id"));
+        if (update.success) {
+          setData({});
+          setLoading(false);
+          return navigate("/add-bus?success=update");
+        } else {
+          setError(true);
+          setLoading(false);
+        }
+      } else {
+        // let array = data.viaPassPoints.split(',')
+        // setData({ ...data, viaPassPoints: array })
+        const add = await addBus(data);
+        if (add.success) {
+          setData({});
+          setLoading(false);
+          return navigate("/add-bus?success=add");
+        } else {
+          setError(true);
+          setLoading(false);
+        }
+      }
+    } catch (err) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    const passPointsInput = document.getElementById("passPoints");
+    if (passPointsInput) {
+      passPointsInput.value = points;
+    }
+  }, [points]);
+  const handlePassPointsChange = (e) => {
+    const inputPoints = e.target.value;
+    if (inputPoints.trim() === "") {
+      setPoints([]);
+    }
+  };
+  const createRoutineMachineLayer = (props) => {
+    const instance = L.Routing.control({
+      waypoints: points.map((point) => L.latLng(point.lat, point.lng)),
+      lineOptions: {
+        styles: [{ color: "#6FA1EC", weight: 4 }],
+      },
+      show: false,
+      addWaypoints: true,
+      routeWhileDragging: true,
+      draggableWaypoints: false,
+      fitSelectedRoutes: true,
+      showAlternatives: true,
+    });
+    return instance;
+  };
 
-export default Add
+  const Routing = createControlComponent(createRoutineMachineLayer);
+
+  return (
+    <Base>
+      <Card style="h-[100vh] w-[1600px] flex flex-col  p-2">
+        <h2 className="text-4xl font-bold ml-2 mt-2">
+          {params.has("id") ? "Update Bus" : "Add Bus"}
+        </h2>
+        {message ? (
+          <p className="ml-2 mt-2 text-sm text-green-600 font-semibold">
+            {message}
+          </p>
+        ) : (
+          ""
+        )}
+        <div className="m-5">
+          <form onSubmit={formSubmit} className="flex flex-col space-y-1">
+            <div className="container m-2 flex flex-col">
+              <label for="route" className="font-semibold text-sm p-2">
+                Route Name *
+              </label>
+              <input
+                onChange={(e) =>
+                  setData({ ...data, routeName: e.target.value })
+                }
+                type="text"
+                id="route"
+                value={data.routeName ? data.routeName : ""}
+                placeholder="Eg- Route 2"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none"
+              />
+            </div>
+            <div className="container m-2 flex flex-col">
+              <label className="font-semibold text-sm p-2">
+                Vehicle Registration *
+              </label>
+              <input
+                onChange={(e) =>
+                  setData({ ...data, vehicleRegistration: e.target.value })
+                }
+                type="text"
+                value={data.vehicleRegistration ? data.vehicleRegistration : ""}
+                placeholder="Eg- UK07PA8077"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none"
+              />
+            </div>
+            <div className="container m-2 flex flex-col">
+              <label className="font-semibold text-sm p-2">Start Point *</label>
+              <input
+                onChange={(e) =>
+                  setData({ ...data, startPoint: e.target.value })
+                }
+                type="text"
+                value={data.startPoint ? data.startPoint : ""}
+                placeholder="Eg- Kandoli"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none"
+              />
+            </div>
+            <div className="container m-2 flex flex-col">
+              <label className="font-semibold text-sm p-2">End Point *</label>
+              <input
+                onChange={(e) => setData({ ...data, endPoint: e.target.value })}
+                type="text"
+                value={data.endPoint ? data.endPoint : ""}
+                placeholder="Eg- Bidholi"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none"
+              />
+            </div>
+            <div className="container m-2 flex flex-col">
+              <label className="font-semibold text-sm p-2">
+                Via Pass Points
+              </label>
+
+              <input
+                id="passPoints"
+                type="text"
+                placeholder="Automatically populated based on map clicks"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover:selection:box-border focus:outline-none"
+                onChange={handlePassPointsChange}
+              />
+            </div>
+            <div className="container m-2 flex flex-col">
+              <label className="font-semibold text-sm p-2">API Key *</label>
+              <input
+                onChange={(e) => setData({ ...data, apiKey: e.target.value })}
+                type="text"
+                value={data.apiKey ? data.apiKey : ""}
+                placeholder="Eg- ljut4k"
+                className="w-[600px] h-[45px] pl-2 border-l-4 border-red-500 text-xl shadow hover: selection:box-border focus:outline-none"
+              />
+            </div>
+
+            {error ? (
+              <p className="text-sm text-red-600">
+                Error Occured! Make Sure You Have Filled All The Details!
+              </p>
+            ) : (
+              ""
+            )}
+            <button
+              disabled={loading ? true : false}
+              type="submit"
+              className="btn w-[300px] h-[45px] bg-[#E9286D] text-center text-white text-xl uppercase m-2 hover:shadow-2xl flex justify-center items-center"
+            >
+              {params.has("id") ? "Update" : "Add Bus"}
+              {!loading ? (
+                ""
+              ) : (
+                <ArrowPathIcon
+                  className="ml-2 animate-spin"
+                  width={30}
+                  height={30}
+                />
+              )}
+            </button>
+          </form>
+        </div>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          className=" w-screen h-[50vh]"
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <LocationMarker />
+          <Routing />
+        </MapContainer>
+      </Card>
+    </Base>
+  );
+};
+
+export default Add;
--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -4,7 +4,10 @@ import Card from "../Components/Card/Card"
 import socketIO from 'socket.io-client'
 import { getToken } from '../Helpers/index'
 import { Link, useSearchParams } from "react-router-dom"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
+import { Icon } from "leaflet";
 
 const Location = () => {
 
@@ -12,6 +15,8 @@ const Location = () => {
     const [location, setLocation] = useState(null)
     const [join, setJoin] = useState(false)
     const [params, setParams] = new useSearchParams()
+    const [loading, setLoading] = useState(true)
+    const [position, setPosition] = useState([30.3165, 78.0322]);
 
     useEffect(() => {
 
@@ -30,90 +35,104 @@ const Location = () => {
             setJoin(true)
         })
 
-        socket.emit('location', params.get('id'))
-        let map, marker;
-        socket.on('locationsent', (item) => {
-            let loc = JSON.parse(item)
-            setData(loc)
-            loc = JSON.parse(loc.lastGpsInformation)
-            window.localStorage.setItem('lat', loc.latitude)
-            window.localStorage.setItem('lng', loc.longitude)
-            setLocation(loc)
-            if(!marker) {
-                marker = new window.mappls.Marker({
-                    map: map,
-                    id: `marker`,
-                    position: [localStorage.getItem('lat') ? localStorage.getItem('lat') : 30.3165, localStorage.getItem('lng') ? localStorage.getItem('lng') : 78.0322],
-                    fitbounds: true,
-                    html: `<div style="white-space:nowrap;font-size:10px;color:#000; background: #fff; width: 60px;">${loc.name}</div>`,
-                    icon_url: 'https://apis.mapmyindia.com/map_v3/1.png'
-                })
-            }else {
-                marker.setPosition({ lat: loc.latitude, lng: loc.longitude})
-                marker.getPosition()
+        // socket.emit('location', params.get('id'))
+        // let map, marker;
+        // socket.on('locationsent', (item) => {
+        //     let loc = JSON.parse(item)
+        //     setData(loc)
+        //     loc = JSON.parse(loc.lastGpsInformation)
+        //     window.localStorage.setItem('lat', loc.latitude)
+        //     window.localStorage.setItem('lng', loc.longitude)
+        //     setLocation(loc)
+        //     if(!marker) {
+        //         marker = new window.mappls.Marker({
+        //             map: map,
+        //             id: `marker`,
+        //             position: [localStorage.getItem('lat') ? localStorage.getItem('lat') : 30.3165, localStorage.getItem('lng') ? localStorage.getItem('lng') : 78.0322],
+        //             fitbounds: true,
+        //             html: `<div style="white-space:nowrap;font-size:10px;color:#000; background: #fff; width: 60px;">${loc.name}</div>`,
+        //             icon_url: 'https://apis.mapmyindia.com/map_v3/1.png'
+        //         })
+        //     }else {
+        //         marker.setPosition({ lat: loc.latitude, lng: loc.longitude})
+        //         marker.getPosition()
                 
-            }
-        }) 
-       
-        const interval = setInterval(() => {
-            socket.emit('location', params.get('id'))      
-        }, 5000)
-        window.initMap1 = () => {
-            map = new window.mappls.Map('map', {
-              center: [30.3165, 78.0322],
-              zoomControl: true,
-              location: true
-            })
-        }
-      
-        const script = document.createElement('script')
-        script.src = 'https://apis.mappls.com/advancedmaps/api/12f73c43d812cd0dc82f3e39b422f960/map_sdk?layer=vector&v=3.0&callback=initMap1'
-        script.defer = true
-        script.async = true
-        document.head.appendChild(script)
-       
-        return () => {
-            socket.disconnect()
-            clearInterval(interval)
-            window.localStorage.clear()
-            document.head.removeChild(script);
-        }
-    }, [])
-   
-    return(
-        <Base>
-            <Card style="h-[80vh] w-[1600px] flex-col flex -ml-2 p-2">
-                <h2 className="text-4xl font-bold ml-5">
-                    Live Location 
-                </h2>
-               
-                <p className="ml-5">{ join ? 'Status: Connected!' : 'Status: Disconnected'}</p>
-                    <div className="flex flex-row flex-wrap gap-5 ml-5">
-                        <h2 className="text-lg border-l-4 border-red-500 pl-2">
-                            <strong>Route Name:</strong> {data ? data.routeName : ''}
-                        </h2>
-                        <h2 className="text-lg border-l-4 border-red-500 pl-2">
-                            <strong>Registration Number:</strong> {data ? data.vehicleRegistration : ''}
-                        </h2>
-                        <h2 className="text-lg border-l-4 border-red-500 pl-2">
-                            <strong>Last Address:</strong> {location ? location.address : ''}
-                        </h2>
-                        <h2 className="text-lg border-l-4 border-red-500 pl-2">
-                            <strong>Last GPS Time:</strong> {location ? location.gpsTimeStr : ''}
-                        </h2>
-                        <h2 className="text-lg border-l-4 border-red-500 pl-2">
-                            <strong>State Of Bus:</strong> {location ? location.statusStr : ''}
-                        </h2>
-                    </div>
-                    <p className="ml-5 text-blue-600">
-                            <Link to={`/add-bus?id=${data ? data._id : ''}`}>Edit Bus</Link>
-                    </p>
-                    <div id="map" className="w-full h-3/4 p-2">
+        //     }
+        // }) 
+         socket.emit("location", params.get("id"));
+         socket.on("locationsent", (item) => {
+           let loc = JSON.parse(item);
+           setData(loc);
+           loc = JSON.parse(loc.lastGpsInformation);
+           setLoading(false);
+           setPosition([loc.latitude, loc.longitude]);
+         });
 
-                    </div>
-            </Card>
-        </Base>
-    )
+         const interval = setInterval(() => {
+           socket.emit("location", params.get("id"));
+         }, 5000);
+
+         return () => {
+           socket.disconnect();
+           clearInterval(interval);
+         };
+    }, [])
+    const myIcon = new Icon({
+      iconUrl: "http://localhost:3000/bus-icon.png",
+      iconRetinaUrl: "http://localhost:3000/bus-icon.png",
+      popupAnchor: [-0, -0],
+      iconSize: [45, 45],
+    });
+   
+    return (
+      <Base>
+        <Card style="h-[80vh] w-[1500px] flex-col flex -ml-2 p-2">
+          <h2 className="text-4xl font-bold ml-5">Live Location</h2>
+
+          <p className="ml-5">
+            {join ? "Status: Connected!" : "Status: Disconnected"}
+          </p>
+          <div className="flex flex-row flex-wrap gap-5 ml-5">
+            <h2 className="text-lg border-l-4 border-red-500 pl-2">
+              <strong>Route Name:</strong> {data ? data.routeName : ""}
+            </h2>
+            <h2 className="text-lg border-l-4 border-red-500 pl-2">
+              <strong>Registration Number:</strong>{" "}
+              {data ? data.vehicleRegistration : ""}
+            </h2>
+            <h2 className="text-lg border-l-4 border-red-500 pl-2">
+              <strong>Last Address:</strong> {location ? location.address : ""}
+            </h2>
+            <h2 className="text-lg border-l-4 border-red-500 pl-2">
+              <strong>Last GPS Time:</strong>{" "}
+              {location ? location.gpsTimeStr : ""}
+            </h2>
+            <h2 className="text-lg border-l-4 border-red-500 pl-2">
+              <strong>State Of Bus:</strong>{" "}
+              {location ? location.statusStr : ""}
+            </h2>
+          </div>
+          <p className="ml-5 text-blue-600">
+            <Link to={`/add-bus?id=${data ? data._id : ""}`}>Edit Bus</Link>
+          </p>
+          <MapContainer
+            className="w-screen h-[50vh]"
+            center={position ? position : [0, 0]}
+            zoom={13}
+            scrollWheelZoom={true}
+          >
+            <TileLayer
+              attribution=""
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {/* <Routing /> */}
+            <Marker icon={myIcon} position={position ? position : [0, 0]}>
+              <Popup>{data ? data.vehicleRegistration : ""}</Popup>
+            </Marker>
+          </MapContainer>
+        </Card>
+      </Base>
+    );
 }
 
 export default Location
